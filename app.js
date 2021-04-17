e829404fb55a767853d27b9733d0426e384e796f
aseelalsayeh <>
2021-04-17 15:58:46 +0000

    new file

---
+++ app.js
@@ -0,0 +1,55 @@
+ //q1
+ var input1 = prompt('please input a number(+ or - )');
+ if (Number(input1) >= 0)
+ {
+   document.write("Positive number");
+ } else {
+   document.write("Negative number");
+ }
+
+//q2
+/*var input1 = prompt("please enter the first number");
+var input2 = prompt("please enter the second number");
+var input3 = prompt("please enter the third number");
+
+if (input1 > input2)
+{
+  if(input1 > input3)
+  {
+     console.log(input1 + " is the max");
+  } else { console.log(input3 + " is the max");}
+
+  
+} else {
+  if (input2 > input3)
+  {
+    console.log(input2 + " is the max");
+  } else {console.log(input3 + " is the max");}
+  }*/
+
+//q3
+  /*var i;
+  var i2=0;
+  for (i=0; i < 20 ; i++){
+    console.log( i + " is even");
+    i2=i+1;
+    console.log (i2 + " is odd");
+    i=i+1;
+  }*/
+
+//q4
+  /*var i;
+  var sum=0;
+  for (i=0;i<=100;i=i+2){
+    sum=sum+i;
+  }
+  console.log(sum);*/
+
+//q5
+  /*var input1 = prompt (" enter the first number");
+  var input2 = prompt ("enter the second number");
+  var sum;
+  sum = Number(input1) + Number(input2);
+  mult = input1 * input2;
+  console.log( 'the sum is' + sum);
+  console.log("the multiplication is" + mult);*/
